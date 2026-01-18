d3aea96034fee870122aa13c2444dc7cbc38507f
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2026-01-18 15:44:24 +0200

    functions for input data and printing board

--- 1-tic-tac-toe.py
+++ 1-tic-tac-toe.py
@@ -1,17 +1,22 @@
-player_one_name = input("Enter the name of Player 1: ")
-player_two_name = input("Enter the name of Player 2: ")
+def read_players_data():
+    player_one_name = input("Enter the name of Player 1: ")
+    player_two_name = input("Enter the name of Player 2: ")
 
-player_one_sign = input(f"{player_one_name}, choose your sign (X/O): ").upper()
-
-while player_one_sign not in ['X', 'O']:
-    print("Invalid choice. Please choose either X or O.")
     player_one_sign = input(f"{player_one_name}, choose your sign (X/O): ").upper()
 
-player_two_sign = 'O' if player_one_sign == 'X' else 'X'
+    while player_one_sign not in ['X', 'O']:
+        print("Invalid choice. Please choose either X or O.")
+        player_one_sign = input(f"{player_one_name}, choose your sign (X/O): ").upper()
 
-print(f"{player_one_name} will be '{player_one_sign}' and {player_two_name} will be '{player_two_sign}'.")
+    player_two_sign = 'O' if player_one_sign == 'X' else 'X'
 
-print("This is the numeration for the Tic Tac Toe board: ")
-print(" 1 | 2 | 3 ")
-print(" 4 | 5 | 6 ")
-print(" 7 | 8 | 9 ")
+    print(f"{player_one_name} will be '{player_one_sign}' and {player_two_name} will be '{player_two_sign}'.")
+    print(f"{player_one_name} starts first.")
+
+    return ((player_one_name, player_one_sign), (player_two_name, player_two_sign))
+
+def print_board_numeration():
+    print("This is the numeration for the Tic Tac Toe board: ")
+    print(" 1 | 2 | 3 ")
+    print(" 4 | 5 | 6 ")
+    print(" 7 | 8 | 9 ")
